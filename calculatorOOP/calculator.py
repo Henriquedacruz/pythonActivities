fc5fc6ba24c264432a4b10fdbb78d2d967c8c3ee
henrique <88168605+Henriquedacruz@users.noreply.github.com>
2022-03-22 16:11:38 -0300

    Create calculator.py

---
+++ calculatorOOP/calculator.py
@@ -0,0 +1,18 @@
+class Calculator:
+
+    def __init__(self, num1, num2):
+        self.resultado = 0
+        self.operando1 = num1
+        self.operando2 = num2
+
+    def somar(self):
+        self.resultado = self.operando1 + self.operando2
+
+    def subtrair(self):
+        self.resultado = self.operando1 - self.operando2
+
+    def multiplicar(self):
+        self.resultado = self.operando1 * self.operando2
+
+    def dividir(self):
+        self.resultado = self.operando1 / self.operando2
