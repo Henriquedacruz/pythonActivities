235d6800edc8065b8a38088397b09599a786bf49
henrique <88168605+Henriquedacruz@users.noreply.github.com>
2022-05-07 19:31:21 -0300

    s

---
+++ Desafio 1.py
@@ -0,0 +1,4 @@
+# Desafio 1 - Converte metros em milimetros
+metros = float(input("Digite um valor em metros:"))  # Recebe o valor e converte para float
+milimetros = metros * 1000
+print("{:.2f}m equivale a {:.2f}mm !!! " .format(metros, milimetros))
